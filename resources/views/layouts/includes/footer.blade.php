589c9a4b6cd2f4a775a48b5d2e5b15cba46f52ff
Taran <21302579+tarantej@users.noreply.github.com>
2021-04-12 08:19:54 +1200

    Add Header, Footer Nav menu Partials and  HTTPS Redirect

---
+++ resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,23 @@
+<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script type="text/javascript" src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/counterup/counterup.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/venobox/venobox.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/typed.js/typed.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('vendor/aos/aos.js')}}"></script>
+
+    <!-- Template Main JS File -->
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+
+     <!-- Material JS -->
+
+     <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
+</body>
+</html>
